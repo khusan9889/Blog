82577cde88481248f5dbfd13e43825e668f493c4
Ulugbek Muslitdinov <ulugbekmuslitdinov@gmail.com>
2021-11-30 16:41:38 +0500

    Post model created

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
